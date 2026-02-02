530f1098f4c9d29f326e3ab157c5bf21cb0dca1b
Selvarajan Thangavel <selvarajan2006@gmail.com>
2026-02-02 22:03:00 +0530

    Initial commit

---
+++ TimeTrace/TimeTraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTraceApp.swift
+//  TimeTrace
+//
+//  Created by Selvarajan on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct TimeTraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
